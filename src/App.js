39b75c27bfd3ed8949b4c09856fbcdd1590d88af
taft1 <108153404+taft1@users.noreply.github.com>
2022-07-12 20:55:47 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
+
 export default function App() {
   return (
-    <div>
-      <h1>Bookkeeper!</h1>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
 }
